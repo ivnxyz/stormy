fc045b1160f490f57fd7e00153533c3bbc8837c6
Iván Martínez <ivanmartinez33@outlook.com>
2017-09-24 18:02:31 -0500

    Initial commit

---
+++ Stormy/Stormy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by Iván Martínez on 24/09/17.
+//  Copyright © 2017 Iván Martínez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
